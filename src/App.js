6db658ad2a73a5bb3a24b7bbec307afba65f35b0
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2023-02-14 03:27:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button type='button' className='btn btn-primary'>
+        Primary
+      </button>
+      <button type='button' className='btn btn-secondary'>
+        Secondary
+      </button>
+      <button type='button' className='btn btn-success'>
+        Success
+      </button>
+      <button type='button' className='btn btn-danger'>
+        Danger
+      </button>
+      <button type='button' className='btn btn-warning'>
+        Warning
+      </button>
+      <button type='button' className='btn btn-info'>
+        Info
+      </button>
+      <button type='button' className='btn btn-light'>
+        Light
+      </button>
+      <button type='button' className='btn btn-dark'>
+        Dark
+      </button>
+
+      <button type='button' className='btn btn-link'>
+        Link
+      </button>
+      <br />
+      <div className='spinner-border text-danger' role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </div>
     </div>
   );
 }
